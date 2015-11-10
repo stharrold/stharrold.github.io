9c7fb941af55a82b4e3a79c661044a31a5bd9705
stharrold <samuel.harrold@gmail.com>
2015-11-10 21:58:20 +0000

    pelican-theme-octopress: added

--- publishconf.py
+++ publishconf.py
@@ -29,5 +29,5 @@ SITEURL = 'https://stharrold.github.io'
 # Feed settings
 # As of 2015-10-30, LinkedIn requires RSS: Profile > Contact Info > Websites
 FEED_DOMAIN = SITEURL
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-FEED_ALL_RSS = 'feeds/all.rss.xml'
+FEED_ATOM = 'feeds/atom.xml'
+FEED_RSS = 'feeds/rss.xml'
