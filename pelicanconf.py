94906fdc9facef1a763548566346b2dec4797678
stharrold <samuel.harrold@gmail.com>
2015-11-11 03:30:18 +0000

    added menu items

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,6 +21,7 @@ import os
 
 # Basic settings
 AUTHOR = 'Samuel Harrold'
+DISPLAY_CATEGORIES_ON_MENU = False
 # Remove all old files and directories when building.
 DELETE_OUTPUT_DIRECTORY = True
 PATH = 'content'
@@ -29,11 +30,13 @@ PATH = 'content'
 # # https://github.com/getpelican/pelican-plugins/tree/master/liquid_tags
 # PLUGINS = ['liquid_tags.notebook']
 # PLUGIN_PATHS = 'pelican-plugins'
-# NOTEBOOK_DIR = 'notebooks'
+NOTEBOOK_DIR = 'notebooks'
 SITENAME = 'Data Science Demos'
 # Define SITEURL only when publishing.
 SITEURL = ''
+STATIC_PATHS = [NOTEBOOK_DIR]
 TIMEZONE = 'Etc/UTC'
+DIRECT_TEMPLATES = ['index', 'categories', 'tags', 'archives', 'authors']
 
 # Feed settings
 # Disable feed generation for developing.
@@ -50,8 +53,13 @@ TRANSLATION_FEED_ATOM = None
 #DISQUS_SITENAME = ''
 GITHUB_URL = 'https://github.com/stharrold/stharrold.github.io'
 #GOOGLE_ANALYTICS = 'UA-XXXX-YYYY'
-SOCIAL = (
+MENUITEMS = [
+    ('Categories', '/categories.html'),
+    ('Tags', '/tags.html'),
+    ('Archives', '/archives.html'),
+    ('Authors', '/authors.html')]
+SOCIAL = [
     ('stharrold', 'https://github.com/stharrold'),
     ('Samuel Harrold', 'https://www.linkedin.com/in/samuelharrold'),
     ('stharrold', 'https://twitter.com/stharrold'),
-    ('Samuel Harrold', 'https://plus.google.com/+SamuelHarrold'),)
+    ('Samuel Harrold', 'https://plus.google.com/+SamuelHarrold')]
